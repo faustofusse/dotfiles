4615fd8c464d824a7cc1d9416f8da5217b7984d4
Fausto Fusse <faustofusse@gmail.com>
2022-08-09 22:56:06 -0300

    treesitter do not install all

--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -3,8 +3,10 @@ if not status_ok then
   return
 end
 
+local languages = { 'go', 'rust', 'python', 'html', 'json', 'javascript', 'julia', 'c', 'css', 'dot', 'dockerfile', 'gomod', 'http', 'kotlin', 'java', 'markdown', 'sql', 'tsx', 'typescript', 'lua', 'vim', 'yaml', 'bash', 'make' }
+
 configs.setup {
-  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = languages, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   -- ignore_install = { "d", "haskell" }, -- List of parsers to ignore installing
   autopairs = {
