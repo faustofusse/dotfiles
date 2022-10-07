51941163efa56831f4fd1fdecb804b0724b87cbf
Fausto Fusse <faustofusse@gmail.com>
2022-10-07 11:53:58 -0300

    mp4 script

--- bin/.local/bin/mp4.sh
+++ bin/.local/bin/mp4.sh
@@ -1,2 +1,2 @@
-http --download "$1" User-Agent:"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36" --output "$2"
+http --download "$1" User-Agent:"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36" --output "$2" --continue
 
