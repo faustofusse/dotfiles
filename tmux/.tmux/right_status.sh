767c79a325f0a1fdae5bd1012339c76783d1c1ee
faustofusse <faustofusse@gmail.com>
2023-06-13 15:50:23 -0300

    feat: tmux wttr fix

--- tmux/.tmux/right_status.sh
+++ tmux/.tmux/right_status.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 function cpu() {
-    printf "cpu: %s" "$(ps -A -o %cpu | awk '{s+=$1} END {print s "%"}')"
+    printf "%s" "$(ps -A -o %cpu | awk '{s+=$1} END {print s "%"}')"
 }
 
 function current_song() {
     printf "#[fg=default]♫  #[fg=default]%s" "$(~/.tmux/spotify-macos.sh info | sed 's/^.*:\ *//g;/^$/d;s/$/;/g' | xargs echo | awk -F "; " '{printf("%s - %s", $2, $1)}' | sed 's/;//g')"
 }
 
+function weather() {
+    curl https://wttr.in/\?format\=%t # +%c
+}
+
 function main() {
     if [ "$(~/.tmux/spotify-macos.sh isrunning)" == "true" ]; then
         current_song
     else
-        cpu
+        printf "%s :: %s" "$(cpu)" "$(weather)"
     fi
 }
 
