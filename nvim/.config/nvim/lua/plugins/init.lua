c95bd6eae9168415e7212542f66789b62e5251b6
faustofusse <faustofusse@gmail.com>
2024-01-21 13:34:11 -0300

    feat(neovim): add mason

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -4,11 +4,8 @@ return {
     { 'kyazdani42/nvim-web-devicons', lazy = true },
     { 'ThePrimeagen/harpoon', lazy = true },
     { 'lewis6991/gitsigns.nvim', opts = {} },
+    { "williamboman/mason.nvim", opts = {} },
     { 'numToStr/Comment.nvim', opts = {}, keys = { { 'gc', mode = { 'n', 'v' } } } },
     { 'windwp/nvim-autopairs', opts = {}, keys = { { '{', mode = 'i' }, { '[', mode = 'i' }, { '<', mode = 'i' }, { '(', mode = 'i' }, { '\'', mode = 'i' }, { '"', mode = 'i' } } },
-    -- themes
-    { 'sainnhe/gruvbox-material', lazy = true },
-    { 'folke/tokyonight.nvim', lazy = true },
-    { 'EdenEast/nightfox.nvim', lazy = true },
     { 'catppuccin/nvim', name = 'catppuccin', lazy = true },
 }
