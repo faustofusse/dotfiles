c8acd57737274b7ab0fc1fc67a6c427d27c46ca3
faustofusse <faustofusse@gmail.com>
2024-07-15 20:57:37 -0300

    feat: checkpoint

--- nvim/.config/nvim/lua/plugins/files.lua
+++ nvim/.config/nvim/lua/plugins/files.lua
@@ -3,12 +3,18 @@ return {
     {
         "stevearc/oil.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
-        keys = { "-" },
         config = function ()
             require("oil").setup {
+                keymaps = {
+                    ["<C-p>"] = false,
+                },
                 view_options = {
                     show_hidden = true,
                 },
+                win_options = {
+                    signcolumn = "yes",
+                },
+                skip_confirm_for_simple_edits = true,
             }
             vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
         end
