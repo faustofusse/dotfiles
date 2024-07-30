796847f1d3ab9c42f9d9029122f5a55ac7c9208e
Fausto Fusse <faustofusse@gmail.com>
2024-07-30 16:08:26 -0300

    feat(zsh): direnv aware icon

--- zsh/.config/zsh/theme.zsh
+++ zsh/.config/zsh/theme.zsh
@@ -1,6 +1,6 @@
 # TODO: hacer algo con %2 en vez de %1 para los que no son git
 
-# Find and set branch name var if in git repository.
+# git branch
 function git_branch_name() {
   branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')
   if [[ $branch == "" ]];
@@ -11,8 +11,19 @@ function git_branch_name() {
   fi
 }
 
+# nix direnv
+function icon() {
+    direnv_file=$DIRENV_FILE
+    if [[ $direnv_file == "" ]];
+    then
+        echo '%#'
+    else
+        echo '#'
+    fi
+}
+
 # Enable substitution in the prompt.
 setopt prompt_subst
 
 # Config for prompt. PS1 synonym.
-prompt=' %1~$(git_branch_name) %# '
+prompt=' %1~$(git_branch_name) $(icon) '
