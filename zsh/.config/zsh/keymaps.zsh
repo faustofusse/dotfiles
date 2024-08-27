35794cdabd437706991eb540549dc960bd366798
Fausto Fusse <faustofusse@gmail.com>
2024-08-27 00:14:22 -0300

    feat(zsh): vim keymaps

--- zsh/.config/zsh/keymaps.zsh
+++ zsh/.config/zsh/keymaps.zsh
@@ -1,3 +1,8 @@
-# history
 bindkey '^[[A' history-substring-search-up # or '\eOA'
 bindkey '^[[B' history-substring-search-down # or '\eOB'
+
+bindkey -v
+bindkey ^R history-incremental-search-backward 
+bindkey ^S history-incremental-search-forward
+bindkey ^P up-line-or-search
+bindkey ^N down-line-or-search
