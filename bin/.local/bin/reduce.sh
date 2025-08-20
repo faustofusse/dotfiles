521588d7205ec04c2df16bd195c27383a864021e
Fausto Fusse <faustofusse@gmail.com>
2025-08-19 21:29:01 -0300

    feat(bin): reduce script should receive one argument and create a bak file

--- bin/.local/bin/reduce.sh
+++ bin/.local/bin/reduce.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-ffpb.sh -y -i "$1" -c:v libx265 -c:a aac -crf 26 "$2"
 # -reset_timestamps 1
 # -threads 1
 # -filter:v fps=60
+
+mv "$1" "$1.bak" && ffpb.sh -y -i "$1.bak" -c:v libx265 -c:a aac -crf 26 "$1" || mv "$1.bak" "$1"
