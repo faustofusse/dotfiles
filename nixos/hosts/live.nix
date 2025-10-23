2f71cdabd8a4031f7bcfb24b786d1467e631f0d2
Fausto Fusse <faustofusse@gmail.com>
2025-10-23 18:42:29 -0300

    feat(nixos): include dotfiles in iso

--- nixos/hosts/live.nix
+++ nixos/hosts/live.nix
@@ -5,10 +5,24 @@
 
   networking.hostName = "faulive";
 
+  # Copy dotfiles into the ISO
+  environment.etc."dotfiles" = {
+    source = ../../.;  # This copies your entire dotfiles repo to /etc/dotfiles
+    mode = "0755";
+  };
+
+  # Deploy dotfiles on first boot
   system.userActivationScripts.dotfiles = {
     text = ''
-      git clone git@github.com:faustofusse/dotfiles ~/.dotfiles
-      stow -d ~/.dotfiles niri zsh ghostty bin apps dunst eww icons mpv nushell nvim opencode tmux tofi yazi zed
+      # Copy dotfiles from /etc to home if not already present
+      if [ ! -d "$HOME/.dotfiles" ]; then
+        cp -r /etc/dotfiles $HOME/.dotfiles
+        chmod -R u+rw $HOME/.dotfiles
+      fi
+      
+      # Use stow to deploy the dotfiles
+      cd $HOME/.dotfiles
+      ${pkgs.stow}/bin/stow -d . niri zsh ghostty bin apps dunst eww icons mpv nushell nvim opencode tmux tofi yazi zed
     '';
     deps = [];
   };
