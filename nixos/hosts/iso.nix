5409691959090f48d2fcada010d0eba2a967b4c1
Fausto Fusse <faustofusse@gmail.com>
2025-10-24 11:34:47 -0300

    feat(nixos): iso!!

--- nixos/hosts/iso.nix
+++ nixos/hosts/iso.nix
@@ -9,7 +9,12 @@
 
   environment.etc.dotfiles = { source = inputs.dotfiles; };
 
-  environment.systemPackages = pkgs.lib.mkAfter [ pkgs.alacritty ];
+  environment.systemPackages = pkgs.lib.mkAfter [
+    (pkgs.runCommand "alacritty-link" { } ''
+     mkdir -p $out/bin
+     ln -s ${pkgs.ghostty}/bin/ghostty $out/bin/alacritty
+     '')
+  ];
 
   system.userActivationScripts.dotfiles = {
     text = ''
@@ -17,7 +22,7 @@
         mkdir -p "$HOME/.dotfiles"
         cp -rL --no-preserve=mode "/etc/dotfiles/." "$HOME/.dotfiles/"
       fi
-      ${pkgs.stow}/bin/stow -d $HOME/.dotfiles niri zsh ghostty bin apps dunst eww icons mpv nushell nvim opencode tmux tofi yazi zed
+      ${pkgs.stow}/bin/stow -d "$HOME/.dotfiles" niri zsh ghostty bin apps dunst eww icons mpv nushell nvim opencode tmux tofi yazi zed
     '';
     deps = [];
   };
--- nixos/readme.md
+++ nixos/readme.md
@@ -1,8 +1,31 @@
 # iso
 
+Crear el ISO
+
+```bash
 nix build .#nixosConfigurations.iso.config.system.build.isoImage
-ls result/iso
-ls /dev/sda*
+```
+
+Formatear un USB
+
+```bash
 sudo dd bs=4M status=progress if=result/iso/nixos-minimal-25.11.20251019.5e2a59a-x86_64-linux.iso of=/dev/sda
+```
+
+Todo junto
+
+```bash
+sudo su && nix build .#nixosConfigurations.iso.config.system.build.isoImage && dd bs=4M status=progress if=result/iso/nixos-minimal-25.11.20251019.5e2a59a-x86_64-linux.iso of=/dev/sda
+```
+
+Para ver los logs del script de activacion (dentro del ISO)
+
+```bash
 journalctl --user --unit=nixos-activation.service
+```
+
+Para correr el ISO en un emulador
+
+```bash
 qemu-system-x86_64 -enable-kvm -m 256 -cdrom result/iso/nixos-minimal-25.11.20251019.5e2a59a-x86_64-linux.iso
+```
